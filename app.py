a66461521e3c54b28f3652aa05307b22cc4f8639
Shreya209 <45393491+Shreya209@users.noreply.github.com>
2020-07-15 15:01:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template, request
+from PIL import Image
+from pytesseract import image_to_string
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = '/static/uploads/'
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# route and function to handle the home page
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+# route and function to handle the upload page
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if there is a file in the request
+       # if 'file' not in request.files:
+        #    return render_template('upload.html', msg='No file selected')
+        file = request.form['img']
+        # if no file is selected
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            image = Image.open(UPLOAD_FOLDER + file.filename, mode='r')
+            Extracted = image_to_string(image)
+   # extract the text and display it
+            return render_template('upload.html',
+                                   extracted_text=Extracted)
+
+    elif request.method == 'GET':
+        return render_template('upload.html')
+
+if (__name__) == ('__main__') :
+    app.run()
